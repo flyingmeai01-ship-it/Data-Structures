8ce96ae782cb9f6e9d314ab15e03ade86214f0cf
flyingmeai01-ship-it <flying.ma.ai.01@gmail.com>
2026-04-20 14:20:04 +0530

    Adding malloc and calloc functions code

---
+++ malloc.c
@@ -0,0 +1,30 @@
+// This is program for memory allocation by using  malloc function
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i, n;
+    printf("Enter how many integers do you wanna store? ");
+    scanf("%d", &n);
+
+// Here we are alloating memory to store integers
+    
+    int *ptr = (int*)malloc(n * sizeof(int));
+    if (ptr == NULL) {
+        printf("Error! memory is not alloated.");
+        exit(1);
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("Enter %d integer: ", i + 1);
+        scanf("%d", ptr + i);
+    }
+
+    printf("These are the %d integers which you stored\n", n);
+    for (i = 0; i < n; i++) {
+        printf("%d ", *(ptr + i));
+    }
+    printf("\n");
+    return 0;
+
+}
