c34852fdae7491d9610cf3b47a096f0c3842d427
flyingmeai01-ship-it <flying.me.ai.01@gmail.com>
2026-04-25 13:03:26 +0530

    Adds node file

---
+++ linked_list/creating_node.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* link;
+};
+
+int main(void) {
+    struct node* head = NULL;
+    head = (struct node*)malloc(sizeof(struct node));
+
+    head -> data = 1;
+    head -> link = NULL;
+
+    printf("Data in Node: %d\n", head -> data);
+    return 0;
+}
