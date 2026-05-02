e1d1ed9233a798024c3ac56026f104797ca22e08
flyingmeai01-ship-it <flying.me.ai.01@gmail.com>
2026-05-03 00:30:46 +0530

    Adding new program by updating recursive method

---
+++ linked_list/add_at_beginningr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* link;
+};
+
+void add_at_beginning(struct node **head, int data) {
+    struct node* ptr = malloc(sizeof(struct node));
+    ptr -> data = data;
+    ptr -> link = *head;
+    *head = ptr;
+}
+
+int main() {
+    struct node *head = malloc(sizeof(struct node));
+    head -> data = 99;
+    head -> link = NULL;
+     
+    struct node *current = malloc(sizeof(struct node));
+    current -> data = 100;
+    current -> link = NULL;
+    head -> link = current;
+    
+    int data = 98;
+    add_at_beginning(&head, data);
+    struct node *ptr = head;
+    while (ptr != NULL) {
+        printf("%d\n", ptr -> data);
+        ptr = ptr -> link;
+    }
+
+    return 0;
+}
