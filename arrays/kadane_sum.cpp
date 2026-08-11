b93d40cda2f61a8c998b364e89aea2d2af1822c6
flyingmeai01-ship-it <flying.me.ai.01@gmail.com>
2026-08-11 13:08:27 +0530

    updating the kadane sum algorithm and adding more test cases

--- arrays/kadane_sum.cpp
+++ arrays/kadane_sum.cpp
@@ -4,7 +4,14 @@
 
 int main() {
     int curSum = 0, maxSum = INT_MIN;
-    std::vector<int> nums = {3, -4, 5, 4, -1, 7, -8};
+    std::vector<int> nums = {-7};
+    // Test cases:
+    // {3, -4, 5, 4, -1, 7, -8}     Output: 15
+    // {-2, -3, -1, -5}             Output: -1             
+    // {-5, -2, 0, -3}              Output: 0
+    // {1, 2, 3, 4}                 Output: 10
+    // {-7}                         Output: -7
+
     int size = sizeof(nums)/sizeof(nums[0]);
 
     for (int i : nums) {
