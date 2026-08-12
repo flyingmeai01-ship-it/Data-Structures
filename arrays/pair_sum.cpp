652ad3cb2a6b48d0e942ebfea25c501de81b38cd
flyingmeai01-ship-it <flying.me.ai.01@gmail.com>
2026-08-12 23:16:30 +0530

    adding pair sum program

--- arrays/pair_sum.cpp
+++ arrays/pair_sum.cpp
@@ -3,9 +3,16 @@
 
 std::vector<int> pair_sum(std::vector<int> arr, int target) {
     std::vector<int> ans;
-    int n = ans.size();
-    
-
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    }
     return ans;
 }
 int main() {
@@ -15,6 +22,5 @@ int main() {
     std::vector<int> arr = {2, 7, 11, 15};
     std::vector<int> ans = pair_sum(arr, target);
 
-    std::cout << "The target indexes are: " << ans[0] << ", " << ans[1] << std::endl;
-    return 0; 
+    std::cout << "Target: " << ans[0] << "," <<  ans[1] << "\n";
 }
